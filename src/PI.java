b2d010c3bd92163c62fa38db47eb474e2e39629b
Hordiichuk Vitalii <vitalikokgo@gmail.com>
2024-04-17 00:59:38 +0300

    Fix out of range

--- src/PI.java
+++ src/PI.java
@@ -6,9 +6,9 @@ public class PI {
         curtask.addJarFile("Pi.jar");
 
         AMInfo info = new AMInfo(curtask, null);
-        point[] p = new point[8];
-        channel[] c = new channel[8];
         int n=100;
+        point[] p = new point[n];
+        channel[] c = new channel[n];
         double step=1.0/n;
         double x=0;
         for (int i=0; i<n; i++) {
