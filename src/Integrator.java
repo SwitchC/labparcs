660ce5ec029552640cd4fae6cc212047f981abff
Hordiichuk Vitalii <vitalikokgo@gmail.com>
2024-04-17 02:36:01 +0300

    Some updates

--- src/Integrator.java
+++ src/Integrator.java
@@ -1,12 +1,14 @@
 import parcs.*;
 public class Integrator implements AM{
     public double integrate(double a,double b){
-        double step=4.0E-323;
-        double sum=0;
-        double x1=a;
-        while(x1<b){
-            sum+=((function(x1)+function(x1+step))*step)/2;
-            x1+=step;
+        double step = 4.9E-324;
+        double sum = 0;
+        double x1 = a;
+        for(int i=0;i<10000;i++) {
+            while (x1 < b) {
+                sum += ((function(x1) + function(x1 + step)) * step) / 2;
+                x1 += step;
+            }
         }
         return sum;
     }
@@ -20,7 +22,5 @@ public class Integrator implements AM{
         double b=info.parent.readDouble();
         double r=integrate(a,b);
         info.parent.write(r);
-        System.out.println(r);
-        System.out.println("End");
     }
 }
