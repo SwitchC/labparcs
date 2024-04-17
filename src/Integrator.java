0f303d85f31852124b0f18b0157f4587858fd3ca
Hordiichuk Vitalii <vitalikokgo@gmail.com>
2024-04-17 03:54:02 +0300

    Change step

--- src/Integrator.java
+++ src/Integrator.java
@@ -1,7 +1,7 @@
 import parcs.*;
 public class Integrator implements AM{
     public double integrate(double a,double b){
-        double step = 4.9E-200;
+        double step = 4.9E-175;
         double sum = 0;
         double x1 = a;
         while (x1 < b) {
